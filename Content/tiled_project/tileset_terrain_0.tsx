8eae18d8617e2429b65477cc36415e00ee32ac28
Andrew Andre Powell <andrewandrepowell2@gmail.com>
2024-04-14 16:17:22 -0500

    Add project files.

---
+++ Content/tiled_project/tileset_terrain_0.tsx
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tileset_terrain_0" tilewidth="32" tileheight="32" tilecount="4096" columns="64">
+ <image source="../tilesets/tileset_terrain_0.png" width="2048" height="2048"/>
+ <wangsets>
+  <wangset name="set_0" type="corner" tile="-1">
+   <wangcolor name="terrains_0" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="terrains_1" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="66" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="67" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="68" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="129" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="130" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="131" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="132" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="133" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="193" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="194" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="195" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="196" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="197" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="257" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="258" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="259" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="260" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="261" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="322" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="323" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="324" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="833" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="834" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="835" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="836" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="897" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="898" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="899" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="900" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="961" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="962" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="963" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="964" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="1474" wangid="0,0,0,2,0,0,0,0"/>
+   <wangtile tileid="1475" wangid="0,0,0,2,0,2,0,0"/>
+   <wangtile tileid="1476" wangid="0,0,0,0,0,2,0,0"/>
+   <wangtile tileid="1537" wangid="0,0,0,2,0,0,0,0"/>
+   <wangtile tileid="1538" wangid="0,2,0,2,0,2,0,0"/>
+   <wangtile tileid="1539" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="1540" wangid="0,0,0,2,0,2,0,2"/>
+   <wangtile tileid="1541" wangid="0,0,0,0,0,2,0,0"/>
+   <wangtile tileid="1601" wangid="0,2,0,2,0,0,0,0"/>
+   <wangtile tileid="1602" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="1603" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="1604" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="1605" wangid="0,0,0,0,0,2,0,2"/>
+   <wangtile tileid="1665" wangid="0,2,0,0,0,0,0,0"/>
+   <wangtile tileid="1666" wangid="0,2,0,2,0,0,0,2"/>
+   <wangtile tileid="1667" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="1668" wangid="0,2,0,0,0,2,0,2"/>
+   <wangtile tileid="1669" wangid="0,0,0,0,0,0,0,2"/>
+   <wangtile tileid="1730" wangid="0,2,0,0,0,0,0,0"/>
+   <wangtile tileid="1731" wangid="0,2,0,0,0,0,0,2"/>
+   <wangtile tileid="1732" wangid="0,0,0,0,0,0,0,2"/>
+  </wangset>
+ </wangsets>
+</tileset>
---
+++ SnakeExtremeGame.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using Microsoft.Xna.Framework.Media;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+using System;
+using System.Collections.Generic;
+#if DEBUG
+using System.Runtime.InteropServices;
+#endif
+
+
+namespace SnakeExtreme
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class SnakeExtremeGame : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Color clearColor = Color.Pink;
+        Texture2D levelTexture2D;
+        TiledMap tiledMap;
+        TiledMapRenderer tiledMapRenderer;
+
+        public SnakeExtremeGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: Use this.Content to load your game content here
+            levelTexture2D = Content.Load<Texture2D>("level_0");
+
+            //tiledMap = Content.Load<TiledMap>("tiled_project/level_0");
+            //tiledMapRenderer = new TiledMapRenderer(graphicsDevice: GraphicsDevice, map: tiledMap);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            MouseState mouseState = Mouse.GetState();
+            KeyboardState keyboardState = Keyboard.GetState();
+            GamePadState gamePadState = default;
+            try { gamePadState = GamePad.GetState(PlayerIndex.One); }
+            catch (NotImplementedException) { /* ignore gamePadState */ }
+
+            if (keyboardState.IsKeyDown(Keys.Escape) ||
+                keyboardState.IsKeyDown(Keys.Back) ||
+                gamePadState.Buttons.Back == ButtonState.Pressed)
+            {
+                try { Exit(); }
+                catch (PlatformNotSupportedException) { /* ignore */ }
+            }
+
+            if (keyboardState.IsKeyDown(Keys.A))
+                clearColor = Color.Blue;
+            if (keyboardState.IsKeyDown(Keys.S))
+                clearColor = Color.Red;
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(clearColor);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.Draw(levelTexture2D, Vector2.Zero, Color.White);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+
+#if DEBUG
+        // https://gamedev.stackexchange.com/questions/45107/input-output-console-window-in-xna#:~:text=Right%20click%20your%20game%20in%20the%20solution%20explorer,tab.%20Change%20the%20Output%20Type%20to%20Console%20Application.
+        // This opens a console window in the game.
+        [DllImport("kernel32")]
+        static extern bool AllocConsole();
+#endif
+    }
+}
