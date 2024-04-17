2113e998e5d91ed7135ffd8b7a6af4c8091a3c50
Andrew Andre Powell <andrewandrepowell2@gmail.com>
2024-04-17 08:16:16 -0500

    Ug. Needed to make another change to MonoGame.Extended in order for the SpriteFactory assembly to properly find the JSON files. Well, that blows. I'll have to eventually include a GitTree of MonoGame.Extended with the changes since I've made several changes to it. At this point, I've started to add most of the animations I want in the snake game.

--- SnakeExtremeGame.cs
+++ SnakeExtremeGame.cs
@@ -2,18 +2,59 @@
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Input.Touch;
-using Microsoft.Xna.Framework.Media;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Sprites;
 using MonoGame.Extended.Tiled;
 using MonoGame.Extended.Tiled.Renderers;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
+using System.Reflection.Metadata;
+using Microsoft.Xna.Framework.Content;
+using MonoGame.Extended;
 
 
 namespace SnakeExtreme
 {
+    public interface IObject
+    {
+        public int Priority { get; }
+        public void Update(GameTime gameTime);
+        public void Draw(SpriteBatch spriteBatch);
+    }
+    public interface ITangible
+    {
+        public Vector2 Position { get; set; }
+        public Size Size { get; }
+    }
+    public class Torch : IObject, ITangible
+    {        
+        private readonly AnimatedSprite sprite;
+        public Torch(ContentManager content)
+        {            
+            var spriteSheet = content.Load<SpriteSheet>("sprite_factory/fire_candelabrum_0.sf", new JsonContentLoader());            
+            sprite = new AnimatedSprite(spriteSheet);
+            sprite.Origin = new Vector2(x: 0, y: 64) - sprite.Origin;
+            sprite.Play("flame_0");
+        }
+        public Vector2 Position { get; set; }
+        public Size Size { get => new Size(width: 32, height: 32); }
+        public int Priority { get => (int)Position.Y; }
+        public void Update(GameTime gameTime)
+        {            
+            sprite.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+            spriteBatch.Draw(sprite: sprite, position: Position);
+            spriteBatch.End();
+        }
+    }
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
@@ -21,10 +62,11 @@ namespace SnakeExtreme
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        Color clearColor = Color.Pink;
-        //Texture2D levelTexture2D;
         TiledMap tiledMap;
         TiledMapRenderer tiledMapRenderer;
+        List<IObject> gameObjects;
+        SpriteSheet spriteSheet;
+        AnimatedSprite sprite;
 
         public SnakeExtremeGame()
         {
@@ -56,10 +98,35 @@ namespace SnakeExtreme
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: Use this.Content to load your game content here
-            //levelTexture2D = Content.Load<Texture2D>("level_0");
             tiledMap = Content.Load<TiledMap>("tiled_project/level_0");
             tiledMapRenderer = new TiledMapRenderer(graphicsDevice: GraphicsDevice, map: tiledMap);
-            GraphicsDevice.BlendState = BlendState.AlphaBlend;
+            var mask0Layer = tiledMap.TileLayers.Where(x => x.Name == "mask_0").First();
+            gameObjects = new();
+            for (int x = 0; x < tiledMap.Width; x++)
+            {
+                for (int y = 0; y < tiledMap.Height; y++)
+                {
+                    var tile = mask0Layer.GetTile((ushort)x, (ushort)y);                    
+                    if (tile.GlobalIdentifier == 4098)
+                    {
+                        var torch = new Torch(Content)
+                        {
+                            Position = new Vector2(x: x * tiledMap.TileWidth, y: y * tiledMap.TileHeight)
+                        };
+                        gameObjects.Add(torch);
+                    }
+                }
+            }
+
+
+            //Console.WriteLine($"CWD: {System.AppDomain.CurrentDomain.BaseDirectory}");
+            //spriteSheet = Content.Load<SpriteSheet>("sprite_factory/fire_candelabrum_0.sf", new JsonContentLoader());
+            //sprite = new AnimatedSprite(spriteSheet);
+            //sprite.Play("flame_0");
+
+            //var mask0Layer = tiledMap.TileLayers.Where(x => x.Name == "mask_0").First();
+            //var tile = mask0Layer.GetTile(10, 10);
+            //Console.WriteLine($"Tile: {tile.GlobalIdentifier}");
         }
 
         /// <summary>
@@ -84,22 +151,18 @@ namespace SnakeExtreme
             try { gamePadState = GamePad.GetState(PlayerIndex.One); }
             catch (NotImplementedException) { /* ignore gamePadState */ }
 
-            if (keyboardState.IsKeyDown(Keys.Escape) ||
-                keyboardState.IsKeyDown(Keys.Back) ||
-                gamePadState.Buttons.Back == ButtonState.Pressed)
-            {
-                try { Exit(); }
-                catch (PlatformNotSupportedException) { /* ignore */ }
-            }
-
-            if (keyboardState.IsKeyDown(Keys.A))
-                clearColor = Color.Blue;
-            if (keyboardState.IsKeyDown(Keys.S))
-                clearColor = Color.Red;
+            //if (keyboardState.IsKeyDown(Keys.Escape) ||
+            //    keyboardState.IsKeyDown(Keys.Back) ||
+            //    gamePadState.Buttons.Back == ButtonState.Pressed)
+            //{
+            //    try { Exit(); }
+            //    catch (PlatformNotSupportedException) { /* ignore */ }
+            //}
 
             // TODO: Add your update logic here
             tiledMapRenderer.Update(gameTime);
-
+            foreach (var gameObject in gameObjects)
+                gameObject.Update(gameTime);
             base.Update(gameTime);
         }
 
@@ -109,10 +172,13 @@ namespace SnakeExtreme
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(clearColor);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
+            GraphicsDevice.BlendState = BlendState.AlphaBlend;
             tiledMapRenderer.Draw();
+            foreach (var gameObject in gameObjects.OrderBy(x => x.Priority))
+                gameObject.Draw(spriteBatch);
             base.Draw(gameTime);
         }
     }
